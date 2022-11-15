22535b711e184e6b2bba6cb0adb8acabeac0eb52
jtspinelli <contato@jonathanspinelli.com>
2022-11-15 19:21:03 -0300

    add favicon

--- back/index.js
+++ back/index.js
@@ -7,6 +7,7 @@ const path = require('path');
 app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use(express.static(path.join(__dirname, '..', 'images')));
 app.use(express.static(path.join(__dirname, '..', 'node_modules')));
 
 app.get('/', (_, res) => {
