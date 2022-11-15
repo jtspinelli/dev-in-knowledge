7a1f1cf3720a1dce797345d8f20d272fa1352bfc
jtspinelli <contato@jonathanspinelli.com>
2022-11-15 09:53:03 -0300

    initial commit

---
+++ back/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = process.env.PORT || 3000;
+const path = require('path');
+
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use(express.static(path.join(__dirname, '..', 'node_modules')));
+
+app.get('/', (_, res) => {
+    res.sendFile(path.join(__dirname, '..', 'index.html'));
+});
+
+app.listen(port, () => {
+    console.log(`APP RUNNING ON PORT ${port}`);
+});
