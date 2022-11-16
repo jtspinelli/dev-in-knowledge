cc0d50040900b2ce164775b54f8f3e8d07f0951a
jtspinelli <contato@jonathanspinelli.com>
2022-11-16 10:15:08 -0300

    add main content

---
+++ public/scripts/form-search.js
@@ -0,0 +1,7 @@
+const formSearch = document.getElementById('form-search');
+
+formSearch.addEventListener('submit', submitFormSearch);
+
+function submitFormSearch(event) {
+    event.preventDefault();
+}
