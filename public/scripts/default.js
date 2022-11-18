4fd46c65f2b21d0bb360852699b152764884d9d3
jtspinelli <contato@jonathanspinelli.com>
2022-11-18 11:23:40 -0300

    add default.js

---
+++ public/scripts/default.js
@@ -0,0 +1,35 @@
+export function getDefaultKnowledges() {
+    return defaultKnowledges;
+}
+
+const defaultKnowledges = [
+    {
+        id: "e31704ca-cfc6-4361-842a-67e37410f9b3",
+        dataCriacao: new Date('Wed Nov 16 2022 17:49:13 GMT-0300 (Brasilia Standard Time)'),
+        titulo: "Título",
+        linguagemSkill: "Algo",
+        categoria: "BackEnd",
+        descricao: "Mussum Ipsum, cacilds vidis litro abertis. Tá deprimidis, eu conheço uma cachacis que pode alegrar sua vidis.Nullam volutpat risus nec leo commodo, ut interdum diam laoreet. Sed non consequat odio. Aenean aliquam molestie leo, vitae iaculis nisl.Casamentiss faiz malandris se pirulitáat odio. Aenean aliquam molestie leo, vitae iaculis nisl.Casamentiss faiz malandris se pirulitá.",
+        youtubeVideo: "HgWldi02m2E",
+    },
+
+    {
+        id: "b74ac328-f9a6-4966-bacc-988331483cf0",
+        dataCriacao: new Date('Wed Nov 16 2022 18:20:13 GMT-0300 (Brasilia Standard Time)'),
+        titulo: "Outro título",
+        linguagemSkill: "Alguma coisa",
+        categoria: "FrontEnd",
+        descricao: "Mussum Ipsum, cacilds vidis litro abertis. Tá deprimidis, eu conheço uma cachacis que pode alegrar sua vidis.Nullam volutpat risus nec leo commodo, ut interdum diam laoreet. Sed non consequat odio. Aenean aliquam molestie leo, vitae iaculis nisl.Casamentiss faiz malandris se pirulitáat odio. Aenean aliquam molestie leo, vitae iaculis nisl.Casamentiss faiz malandris se pirulitá.",
+        youtubeVideo: null,
+    },
+
+    {
+        id: "017c13a1-3ad9-4ed2-b9a2-a30903b58aec",
+        dataCriacao: new Date('Thu Nov 17 2022 09:51:19 GMT-0300 (Brasilia Standard Time)'),
+        titulo: "Dica tal e tal",
+        linguagemSkill: "Alguma coisa",
+        categoria: "FrontEnd",
+        descricao: "Mussum Ipsum, cacilds vidis litro abertis. Tá deprimidis, eu conheço uma cachacis que pode alegrar sua vidis.Nullam volutpat risus nec leo commodo, ut interdum diam laoreet. Sed non consequat odio. Aenean aliquam molestie leo, vitae iaculis nisl.Casamentiss faiz malandris se pirulitáat odio. Aenean aliquam molestie leo, vitae iaculis nisl.Casamentiss faiz malandris se pirulitá.",
+        youtubeVideo: 'dq76BeYmc-Y',
+    }
+];
